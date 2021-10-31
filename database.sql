33e9c6a9bd47f7a0067484378c6a11de2336da23
EinarLop <36113521+Einar-Lop@users.noreply.github.com>
2021-10-30 20:10:16 -0500

    first commit

---
+++ database.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS dines ;
+
+CREATE TABLE dines(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    score INT NOT NULL,
+    restaurant VARCHAR(100) NOT NULL,
+    address VARCHAR(200) NOT NULL,
+    date DATE NOT NULL, 
+    image VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
